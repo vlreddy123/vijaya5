f13e7e9b79b635e9a455085a807506f00519f88c
vlreddy123 <99265985+vlreddy123@users.noreply.github.com>
2022-02-09 20:46:53 +0530

    Create second.py

---
+++ second.py
@@ -0,0 +1,11 @@
+=int(input('enter the value of a:')) 
+ ​b=int(input('enter the value of b:')) 
+ ​if a>=b: 
+ ​  print('a is biggest number') 
+ ​else:  
+ ​  print('a is smallest number')
+___________
+____________      
+ Output  enter the value of a:10 
+   enter the value of b: 5   
+     a is the biggest number
