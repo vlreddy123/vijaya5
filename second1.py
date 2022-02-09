a5c8f9e61239bc643ee54a2d1ccf54b735b1afc1
vlreddy123 <99265985+vlreddy123@users.noreply.github.com>
2022-02-09 21:14:56 +0530

    Create second1.py

---
+++ second1.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: ")) 
+ if (num % 2) == 0: 
+    print("the given number is Even",num) 
+ else: 
+   print("the given number is Odd",num)     
+     Output
+______________
+______________  
+0utput:
+Enter a number:5
+the given number is 0dd
